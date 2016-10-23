19009cf6f43453f73f49fb2fc1620a8a8ae7f152
Ty <Ty_Coghlan@hms.harvard.dedu>
2016-10-23 07:53:33 -0400

    updated service

--- rest/src/main/scala/Bar.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by ty on 10/23/16.
-  */
-object Bar {
-  def main(args: Array[String]): Unit = {
-    println("Seriously, just work please.")
-  }
-}
--- rest/src/main/scala/PavlovService.scala
+++ rest/src/main/scala/PavlovService.scala
@@ -1,7 +1,11 @@
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import spray.json._
+import scala.concurrent.Future
+
 
 
 trait PavlovService extends SprayJsonSupport with DefaultJsonProtocol {
@@ -9,6 +13,7 @@ trait PavlovService extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val answerFormat = jsonFormat1(Answer)
   implicit val accountsFormat = jsonFormat1(Accounts)
   implicit val accountFormat = jsonFormat1(Account)
+  implicit val paidFormat = jsonFormat1(Paid)
 
   case class Text(text: String)
 
@@ -17,6 +22,8 @@ trait PavlovService extends SprayJsonSupport with DefaultJsonProtocol {
   case class Accounts(accounts: List[String])
 
   case class Account(account: String)
+
+  case class Paid(paid: Double)
 
   def route: Route = get {
     path("ping") {
@@ -34,9 +41,18 @@ trait PavlovService extends SprayJsonSupport with DefaultJsonProtocol {
 
   def accounts: Route = {
     path("accounts") {
-      parameter("account".as[String]) { accounts: String =>
-        complete(Accounts(List("123456", "142524", "135246")))
+      parameter("account".as[String]) { accounts: String => {
+        complete(Accounts(List("580c9f98360f81f10454502f", "580c9f38360f81f10454502c", "580c9f74360f81f10454502d", "580c9ee6360f81f104545028")))
+        }
       }
+    }
+  }
+
+  def pay: Route = {
+    path("pay") {
+      parameters("account".as[String], "amount".as[String]) { (account:String, amount:String) => {
+        complete(Paid(amount.toDouble / 4.0))
+      }}
     }
   }
 }
